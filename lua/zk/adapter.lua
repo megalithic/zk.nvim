2f2392648629cda837fa7156881db70e32bc95ea
Seth Messer <seth.messer@gmail.com>
2021-03-16 10:34:30 -0500

    Adds some additional logging for stdout, stderr, exit

--- lua/zk/adapter.lua
+++ lua/zk/adapter.lua
@@ -9,17 +9,42 @@ function M.new(title)
     cmd = string.format("zk new --title %s", title)
   end
 
-  vim.fn.jobstart(cmd, {
-    on_exit = function(j, d, e)
-    print(string.format("[zk.nvim] on_job_exit -> j: %s, d: %s, e: %s", vim.inspect(j), vim.inspect(d), vim.inspect(e)))
-      if d == 0 then
-        vim.api.nvim_out_write("[zk.nvim] new zettel note created")
-        return
-      end
+  vim.fn.jobstart(
+    cmd,
+    {
+      on_stdout = function(j, d, e)
+        print(
+          string.format("[zk.nvim] on_stdout -> j: %s, d: %s, e: %s", vim.inspect(j), vim.inspect(d), vim.inspect(e))
+        )
+        if d == 0 then
+          vim.api.nvim_out_write("[zk.nvim] new zettel note created")
+          return
+        end
 
-      vim.api.nvim_err_writeln("[zk.nvim] failed to create new zettel note")
-    end,
-  })
+        vim.api.nvim_err_writeln("[zk.nvim] failed to create new zettel note")
+      end,
+      on_stderr = function(j, d, e)
+        print(
+          string.format("[zk.nvim] on_stderr -> j: %s, d: %s, e: %s", vim.inspect(j), vim.inspect(d), vim.inspect(e))
+        )
+        if d == 0 then
+          vim.api.nvim_out_write("[zk.nvim] new zettel note created")
+          return
+        end
+
+        vim.api.nvim_err_writeln("[zk.nvim] failed to create new zettel note")
+      end,
+      on_exit = function(j, d, e)
+        print(string.format("[zk.nvim] on_exit -> j: %s, d: %s, e: %s", vim.inspect(j), vim.inspect(d), vim.inspect(e)))
+        if d == 0 then
+          vim.api.nvim_out_write("[zk.nvim] new zettel note created")
+          return
+        end
+
+        vim.api.nvim_err_writeln("[zk.nvim] failed to create new zettel note")
+      end
+    }
+  )
 end
 
 function M.search()
